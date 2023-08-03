3224ebb643ca91e77123f0480904f55a4385c63f
Max Neuvians <max@neuvians.net>
2023-08-03 21:18:25 +0000

    test: sleep

--- dist/index.js
+++ dist/index.js
@@ -3479,18 +3479,24 @@ const { spawn, exec } = __nccwpck_require__(81);
 const fs = __nccwpck_require__(147);
 const pcap_parser = __nccwpck_require__(642);
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const main = () => {
     // Check if dns.pcap exists
     if (!fs.existsSync("dns.pcap")) {
 
         const command = 'sudo';
-        const args = ['tcpdump', '-n', '-w', 'dns.pcap', 'port', '53'];
+        const args = ['tcpdump', '-n', '-l', '-w', 'dns.pcap', 'port', '53'];
 
         // Start the child process
         const tcpdumpProcess = spawn(command, args, {
             detached: true, // Detach the child process from the parent
             stdio: 'ignore', // Ignore stdin, stdout, and stderr
         });
+
+        sleep(5000);
 
         // Unref the child process to allow the parent process to exit
         tcpdumpProcess.unref();
