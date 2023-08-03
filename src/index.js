83e9cff05d601dcf86feab47f85caae55ae74366
Max Neuvians <max@neuvians.net>
2023-08-03 20:11:53 +0000

    fix: pais

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const main = () => {
     // Check if dns.pcap exists
     if (!fs.existsSync("tmp/dns.pcap")) {
         // Start TCPDump
-        exec.exec('sudo', ['tcpdump', '-n', '-w', 'tmp/dns.pcap', 'port', '53']);
+        exec.exec('sudo', ['tcpdump', '-n', '-w tmp/dns.pcap', 'port 53']);
     } else {
         // Kill all TCPDump processes
         exec.exec('sudo', ['killall', 'tcpdump']);
